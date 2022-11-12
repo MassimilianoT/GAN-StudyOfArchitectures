14c65e8089859324be5dad065973e3ef3f9bb642
MassimilianoT <m.tummolo@studenti.unibs.it>
2022-11-12 12:25:17 +0100

    errata corrige

--- dcgan/train_model.py
+++ dcgan/train_model.py
@@ -4,6 +4,6 @@ answer = input('Usare CelebA o Mnist? (C per celebA, M per Mnist)')
 if answer is 'C':
     train_DCGAN(use_celebA=True)
 elif answer is 'M':
-    train_DCGAN()
+    train_DCGAN(use_celebA=False)
 else:
     print('Inserito carattere non corretto. Riprovare')
